e136e42dfcc36b50f3d526075cb0e836e5f53f67
jihee-daily <kim76955@gmail.com>
2024-06-17 10:46:28 +0900

    init

---
+++ PAWS/PAWS/PAWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAWSApp.swift
+//  PAWS
+//
+//  Created by kimjihee on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PAWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
